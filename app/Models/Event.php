514bb4866bd19d5836450047474ad37254c08f20
aichaettabet <aicha70tabite@gmail.com>
2024-03-05 22:13:23 +0100

    model and migaration done

---
+++ app/Models/Event.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'description', 'date', 'location', 'category_id', 'available_seats'];
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
